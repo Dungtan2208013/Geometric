abe244e163cea4ace574143fa7094c7e72ef503c
DungTan <dungptth2208013@fpt.edu.vn>
2023-05-24 09:10:38 +0700

    .

---
+++ Abstract1`/GeometricObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class GeometricObject
+{
+    protected string color;
+    protected double weight;
+    protected GeometricObject()
+    {
+        color = "white";
+        weight = 1.0;
+    }
+    //Construct a GeometricObject object
+    protected GeometricObject(string color, double weight)
+    {
+        this.color = color;
+        this.weight = weight;
+    }
+    // properties
+    public string Pcolor
+    {
+        get { return color; }
+        set { color = value; }
+    }
+    public double PWeight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+    //Abstract method
+    public abstract double findArea();
+    public abstract double findPerimeter();
+}
+
+public class Circle : GeometricObject
+{
+    private double radius;
+    public Circle(double x) 
+    {
+        this.radius = x;
+    }
+    public Circle(double x, string c,double w) :base (c,w)
+    {
+        this.radius = x;
+    }
+    public override string ToString()
+    {
+        return "Circle has: radius is" + radius + ", color is " + Pcolor + ", weight is" + PWeight;
+    }
+    public override double findArea()
+    {
+        return Math.PI*radius*radius;
+    }
+    public override double findPerimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+
+
+}
+
---
+++ Abstract1`/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(2.45,"Blue",23);
+        Console.WriteLine("Circle before change:" + c1.ToString());
+
+        //using properties
+        c1.PWeight = 2.56;
+        c1.Pcolor = "red";
+
+        Console.WriteLine("Cricle after change:" + c1.ToString());
+        Console.ReadLine();
+        }
+    }
